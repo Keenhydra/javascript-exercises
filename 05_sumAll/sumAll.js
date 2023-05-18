3327517c8ddd220b3b579b6ebb655084a47322ec
keenhydra <62450181+Keenhydra@users.noreply.github.com>
2023-05-18 17:49:55 +0200

    Complete exercise 5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(...numbers) {
+numbers.sort(function(a, b){return a - b});
+first = numbers[0];
+last = numbers[1];
+let n = first;
+let x = first;
 
+
+if ((typeof numbers[0] !== 'number') || (typeof numbers[1] !== 'number')){
+    return "ERROR";
+}else if (first >= 0){
+        while (n < last) {
+        n++;
+        x += n;
+        }
+}else {
+    return "ERROR";
+}
+
+total = x;
+return total;
 };
 
 // Do not edit below this line
