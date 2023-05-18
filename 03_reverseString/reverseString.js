5d4f7b90bf7f82ddff58fc9699effafcb84fad4f
keenhydra <62450181+Keenhydra@users.noreply.github.com>
2023-05-18 14:24:02 +0200

    Complete first four exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = Array.from(string);
+    arr2 = arr.reverse();
+    output = arr2.join("");
+    return output;
 };
+
+//string to array
+//flip array
+//array to string
+
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) {
+    let length = array.length;
+    let arglength = theArgs.length;
+    arr = array;
+  
+    for (i = 0; i < length; i++){
+        for (j = 0; j < arglength; j++)   
+            if (theArgs[j] === arr[i]){
+            arr.splice(i, 1), i--;
+        }
+    }
+    return arr;
 };
+
+//import array
+//check rem to subject array
+//remove if match
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
