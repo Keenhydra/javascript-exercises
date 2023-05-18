c6a616ab80dce1ae29d30e67f705df90131f9684
keenhydra <62450181+Keenhydra@users.noreply.github.com>
2023-05-18 21:01:31 +0200

    Complete tempConversion exercise.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  celsius = (farenheit - 32) * (5/9);
+  decimal = celsius.toFixed(1);
+  
+  return Number(decimal);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  farenheit = (celsius * (9/5) + 32); 
+  decimal = farenheit.toFixed(1);
+  
+  return Number(decimal);
 };
 
 // Do not edit below this line
