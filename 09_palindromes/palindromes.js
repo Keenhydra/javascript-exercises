e7e4fe519f51c8bf3346dfdbe08cb817792d4137
keenhydra <62450181+Keenhydra@users.noreply.github.com>
2023-05-19 00:15:43 +0200

    Complete palindromes assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const regex = /!,. /g;
+    stripped = word.toLowerCase("").replaceAll(regex, "");
+    inverse = stripped.split("").reverse().join("");
+    console.log(regex);
+    if (stripped === inverse){
+        return true;
+    }else {
+        return false;
+    }
 };
 
 // Do not edit below this line
