93060303ebf14225ea93248ff3eb785476fe0387
keenhydra <62450181+Keenhydra@users.noreply.github.com>
2023-05-18 18:22:51 +0200

    Complete leapYears assignment

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+if((year%4) == 0){
+    if(year%100 == 0){
+        if((year%400 == 0) && (year%100 == 0)){
+            return true
+        }else{
+            return false
+        }
+    } else {
+        return true
+    }
+} else {
+    return false;
+}
 
 };
+
+//year must %4
+//if year %100 > year must %400 => true
 
 // Do not edit below this line
 module.exports = leapYears;
