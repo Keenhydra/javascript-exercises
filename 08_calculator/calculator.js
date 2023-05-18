2e441ef534eee7a5dfb5f9d95e90cdc3aa04c89b
keenhydra <62450181+Keenhydra@users.noreply.github.com>
2023-05-18 22:39:28 +0200

    Complete calculator assignment

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	result = num1 + num2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	result = num1 - num2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(myNums) {
+  total = 0;
+  for (let i = 0; i < myNums.length; i++) {
+    total += myNums[i];
+  }
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(myNums) {
+    total = 1;
+    for (let i = 0; i < myNums.length; i++) {
+      total *= myNums[i];
+    }
+    return total;
+  };
 
+const power = function(num, level) {
+	return total = num ** level;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  total = 1;
+	for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
