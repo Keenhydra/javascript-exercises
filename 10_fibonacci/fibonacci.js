db7460a563cbac6cd0e86d77b8aae7882129c483
keenhydra <62450181+Keenhydra@users.noreply.github.com>
2023-05-19 13:04:26 +0200

    Complete fibonacci assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0){
+        return "OOPS";
+    }
+    sequence=1;
+    number = Number(num);
+    n1 = 0;
+    n2 = 1;
+    for (i=1; i < number; i++){
+        sequence = n1 + n2;
+        n1 = n2;
+        n2 = sequence;
+    }
+    return sequence;
 };
 
 // Do not edit below this line
