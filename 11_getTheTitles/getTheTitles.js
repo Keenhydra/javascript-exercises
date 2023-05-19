8835e1641f567838c0c5ba956bf5d478f7e3405e
keenhydra <62450181+Keenhydra@users.noreply.github.com>
2023-05-19 20:21:47 +0200

    Complete getTheTitles assignment

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+    let titles = [books[0].title, books[1].title];
+    console.log(titles);
+    return titles;
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
