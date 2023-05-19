8a8a7893a50cfee6fa9e8b06a6916eadd8aa02de
keenhydra <62450181+Keenhydra@users.noreply.github.com>
2023-05-19 22:12:03 +0200

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    
+    
+    let currentYear = new Date().getFullYear();
 
+    people.sort(function(b, a)
+    {return ((a.yearOfDeath??currentYear) - a.yearOfBirth) - ((b.yearOfDeath??currentYear) - b.yearOfBirth)});
+    
+    console.log(people)
+    return people[0];
 };
 
 // Do not edit below this line
